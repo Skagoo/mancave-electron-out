ce054efd48ccf4d4c8cbfa0ef02c08b9b1048f41
Sacha <dhr.vandenwyngaert@outlook.com>
2018-10-13 12:51:39 +0200

    remove .gitignore

---
+++ ManCave-win32-x64/resources/app/app/js/couchdb.js
@@ -0,0 +1,124 @@
+const NodeCouchDb = require('node-couchdb'); // https://www.npmjs.com/package/node-couchdb
+
+const databaseName = 'mancave-electron';
+
+// node-couchdb instance talking to external service
+const couch = new NodeCouchDb({
+    host: 'skagoo.com',
+    protocol: 'https',
+    port: 6984,
+    auth: {
+        user: 'admin',
+        pass: 'couchdb759153'
+    }
+});
+
+function getTodaysDocID() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd = '0'+dd
+    } 
+
+    if(mm<10) {
+        mm = '0'+mm
+    } 
+
+    today = mm + '-' + dd + '-' + yyyy;
+
+    return today;
+}
+
+function createTodaysDoc() {
+    couch.insert(databaseName, {
+        _id: getTodaysDocID(),
+        messages: []
+    }).then(({data, headers, status}) => {
+        // data is json response
+        // headers is an object with all response headers
+        // status is statusCode number
+    }, err => {
+        // either request error occured
+        // ...or err.code=EDOCCONFLICT if document with the same id already exists
+    });
+}
+  
+function loadTodaysChat() {
+    var docID = getTodaysDocID();
+
+    couch.get(databaseName, docID).then(({data, headers, status}) => {
+        // data is json response
+        // headers is an object with all response headers
+        // status is statusCode number
+        var doc = data;
+        
+        for (let index = 0; index < doc.messages.length; index++) {
+            var templateID = doc.messages[index].templateID;
+            var context = doc.messages[index].context;
+            var template;
+
+            if (context.senderUID =! selfClientUID) { // Message was not sent by self
+                templateID = templateID.replace('-template', '-response-template');
+            }
+            
+            // Show
+            template = Handlebars.compile($(templateID).html());
+            chat.$chatHistoryList.append(template(context));
+            chat.scrollToBottom();
+        }
+        chat.scrollToBottom();
+
+    }, err => {
+        // either request error occured
+        // ...or err.code=EDOCMISSING if document is missing
+        // ...or err.code=EUNKNOWN if statusCode is unexpected
+        if(err.code == 'EDOCMISSING') {
+            createTodaysDoc();
+        }
+        else {
+            console.log(err);
+        }
+    });
+}
+
+function addChatMessage(messageObj) {
+    messageObj.context.messageContent = messageObj.context.messageContent.replace(`"`, `\"`);
+
+    var docID = getTodaysDocID();
+
+    couch.get(databaseName, docID).then(({data, headers, status}) => {
+        // data is json response
+        // headers is an object with all response headers
+        // status is statusCode number
+        var doc = data;
+        doc.messages.push(messageObj);
+
+        // note that "doc" must have both "_id" and "_rev" fields
+        couch.update(databaseName, {
+            _id: doc._id,
+            _rev: doc._rev,
+            messages: doc.messages
+        }).then(({data, headers, status}) => {
+            console.log('status' + status);
+        }, err => {
+            // either request error occured
+            // ...or err.code=EFIELDMISSING if either _id or _rev fields are missing
+            console.log(err);
+        });
+
+    }, err => {
+        // either request error occured
+        // ...or err.code=EDOCMISSING if document is missing
+        // ...or err.code=EUNKNOWN if statusCode is unexpected
+        if(err.code == 'EDOCMISSING') {
+            createTodaysDoc();
+            addChatMessage(messageObj);
+        }
+        else {
+            console.log(err);
+        }
+    });
+}
